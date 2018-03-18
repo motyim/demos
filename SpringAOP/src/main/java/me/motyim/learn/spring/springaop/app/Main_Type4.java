99c1b4cbdf93d8863c552ce75e59c6eaf825bfae
Mohamed MotYim <mohamed.motyim@gmail.com>
2018-03-18 23:24:15 +0200

    add type 3 & 4

---
+++ SpringAOP/src/main/java/me/motyim/learn/spring/springaop/app/Main_Type4.java
@@ -0,0 +1,24 @@
+package me.motyim.learn.spring.springaop.app;
+
+import me.motyim.learn.spring.springaop.classes.Calculator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since Mar 18, 2018
+ */
+public class Main_Type4 {
+
+    public static void main(String[] args) {
+        
+        ApplicationContext app = new ClassPathXmlApplicationContext("config_type3.xml");
+        Calculator calculator = (Calculator)app.getBean("proxy");
+        
+        calculator.add(2, 3);
+        
+        System.out.println("--------------- MAIN --------------");
+    
+        calculator.div(2, 0);
+    }
+}
