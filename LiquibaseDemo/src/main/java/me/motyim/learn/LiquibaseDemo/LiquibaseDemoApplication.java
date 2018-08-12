e2ad52c4d105390e2981d23f74e5914da0ea4804
motyim <mohamed.motyim@gmail.com>
2018-08-12 17:57:29 +0200

    add LiquibaseDemo

---
+++ LiquibaseDemo/src/main/java/me/motyim/learn/LiquibaseDemo/LiquibaseDemoApplication.java
@@ -0,0 +1,12 @@
+package me.motyim.learn.LiquibaseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+}
---
+++ LiquibaseDemo/src/test/java/me/motyim/learn/LiquibaseDemo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package me.motyim.learn.LiquibaseDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiquibaseDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
