74c0ae0260056c33c3b735818dea1a009c619c3c
motyim <mohamed.motyim@gmail.com>
2020-01-22 22:51:37 +0300

    add integeration test

---
+++ docker-micro-service/page-view-client/src/test/java/me/motyim/docker/pageviewclient/FakeIntegrationTestIT.java
@@ -0,0 +1,40 @@
+package me.motyim.docker.pageviewclient;
+
+import org.junit.jupiter.api.Test;
+
+public class FakeIntegrationTestIT {
+
+    @Test
+    public void testFakeSeniro1() throws InterruptedException {
+        System.out.println("Fake Test 1 Run .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . . . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 1 Run . . . . . . .");
+    }
+
+    @Test
+    public void testFakeSeniro2() throws InterruptedException {
+        System.out.println("Fake Test 2 Run .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . . . . . .");
+        Thread.sleep(100);
+        System.out.println("Fake Test 2 Run . . . . . . .");
+    }
+}
